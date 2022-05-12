389e8fb060e22e4acc9b173d3b77dd6013c5d0bd
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-11 19:11:46 -0700

    pair programming exercise completed with Trevor

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,12 +15,12 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present HORIZONTALLY", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,8 +31,27 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word is present VERTICALLY", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['A', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['R', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['D', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['V', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['A', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['R', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['K', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['S', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'AARDVARKS');
+    assert.isTrue(result);
+  });
+  it("should return UNDEFINED if the word matrix is empty", function() {
+    const result = wordSearch([['']], 'WORD');
+    assert.isUndefined;
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+    let newArr = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+        let arr = [];
+        for (let j = 0; j < matrix.length; j++) {
+            arr.push(matrix[j][i]);
+        }
+        newArr.push(arr);
     }
-}
+    return newArr;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+    if (letters.length < 1) {
+        return undefined;
+    };
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (l of horizontalJoin) {
+        console.log(l);
+        if (l.includes(word)) return true;
+    }
+    const tr = transpose(letters);
+    const verticalJoin = tr.map(x => x.join(''));
+    for (l of verticalJoin) {
+        console.log("vertical", l);
+        if (l.includes(word)) return true;
+    }
+    return false;
+};
+
+
+
+
+module.exports = wordSearch;
